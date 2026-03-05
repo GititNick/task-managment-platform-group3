368160da0229de366de6c2f7951bf7f6dcbf1020
Kofo <ladi5951@freddonia.edu>
2026-03-05 12:06:58 -0500

    Added layout, navbar, and dashboard

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark px-4">
+      <span className="navbar-brand mb-0 h1">
+        Task Manager
+      </span>
+    </nav>
+  );
+}
+
+export default Navbar;
